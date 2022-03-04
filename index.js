41d346e9c2483cf79dc0139f01d37cebab637a59
Deependra Solanki <deepsolanki3010@gmail.com>
2022-03-04 18:19:48 +0530

    Added Error Handling (utils folder, error.ejs file)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const methodOverride = require('method-override')
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const mongoose = require('mongoose');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressErrors');
 const Forum = require('./models/forum');
 
 const categories = ['Exam', 'University', 'Engineering', 'Management', 'Programming', 'Placements', 'Other'];
@@ -29,36 +31,48 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/questions', async (req, res) => {
+app.get('/questions', catchAsync(async (req, res) => {
     const questions = await Forum.find({});
     res.render('Questions/index', { questions });
-});
+}));
 app.get('/questions/new', (req, res) => {
     res.render('Questions/new', { categories })
 });
-app.post('/questions', async (req, res) => {
+app.post('/questions', catchAsync(async (req, res) => {
+    if (!req.body.question) throw new ExpressError('Invalid Question Data', 400);
     const question = new Forum(req.body.question);
     await question.save();
     res.redirect(`/questions/${question._id}`)
-})
-app.get('/questions/:id', async (req, res,) => {
+}));
+app.get('/questions/:id', catchAsync(async (req, res,) => {
     const question = await Forum.findById(req.params.id)
     res.render('Questions/show', { question });
-});
-app.get('/questions/:id/edit', async (req, res) => {
+}));
+app.get('/questions/:id/edit', catchAsync(async (req, res) => {
     const question = await Forum.findById(req.params.id)
     res.render('Questions/edit', { question, categories });
-});
-app.put('/questions/:id', async (req, res) => {
+}));
+app.put('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const question = await Forum.findByIdAndUpdate(id, { ...req.body.question });
     res.redirect(`/questions/${question._id}`);
-});
-app.delete('/questions/:id', async (req, res) => {
+}));
+app.delete('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Forum.findByIdAndDelete(id);
     res.redirect('/questions');
-});
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found!!', 404));
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!!'
+    res.status(statusCode).render('error', { err });
+})
+
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000!!!');
