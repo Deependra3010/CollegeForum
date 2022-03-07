099ec1b63e1a7191c6ba9335f77ff73fd8c9b99d
Deependra Solanki <deepsolanki3010@gmail.com>
2022-03-07 23:19:21 +0530

    added session, flash and pulbic directory

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressErrors');
 const Forum = require('./models/forum');
 const Answer = require('./models/answer');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const categories = ['Exam', 'University', 'Engineering', 'Management', 'Programming', 'Placements', 'Other'];
 
@@ -26,8 +28,29 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+const sessionConfig = {
+    secret: 'thisisasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -43,29 +66,35 @@ app.post('/questions', catchAsync(async (req, res) => {
     if (!req.body.question) throw new ExpressError('Invalid Question Data', 400);
     const question = new Forum(req.body.question);
     await question.save();
+    req.flash('success', 'We hope you get an Answer soon!');
     res.redirect(`/questions/${question._id}`)
 }));
-<<<<<<< HEAD
-app.get('/questions/:id', catchAsync(async (req, res,) => {
-    const question = await Forum.findById(req.params.id).populate('answers');
-=======
 app.get('/questions/:id', catchAsync(async (req, res) => {
-    const question = await Forum.findById(req.params.id)
->>>>>>> 5785c587294d0d2adb91f1bdfa4f28a38d43be9f
+    const question = await Forum.findById(req.params.id);
+    if (!question) {
+        req.flash('error', 'Connot find the Campground!');
+        return res.redirect('/questions');
+    }
     res.render('Questions/show', { question });
 }));
 app.get('/questions/:id/edit', catchAsync(async (req, res) => {
-    const question = await Forum.findById(req.params.id)
+    const question = await Forum.findById(req.params.id);
+    if (!question) {
+        req.flash('error', 'Connot find the Campground!');
+        return res.redirect('/questions');
+    }
     res.render('Questions/edit', { question, categories });
 }));
 app.put('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const question = await Forum.findByIdAndUpdate(id, { ...req.body.question });
+    req.flash('success', 'Successfully edited the Question!')
     res.redirect(`/questions/${question._id}`);
 }));
 app.delete('/questions/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Forum.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted the Question!')
     res.redirect('/questions');
 }));
 
@@ -75,12 +104,14 @@ app.post('/questions/:id/answers', catchAsync(async (req, res) => {
     question.answers.push(answer);
     await answer.save();
     await question.save();
+    req.flash('success', 'Thankyou For Answering the Question!')
     res.redirect(`/questions/${question._id}`)
 }));
 app.delete('/questions/:id/answers/:answerId', catchAsync(async (req, res) => {
     const { id, answerId } = req.params;
     await Forum.findByIdAndUpdate(id, { $pull: { answers: answerId } });
     await Answer.findByIdAndDelete(answerId);
+    req.flash('success', 'Successfully deleted the Question!')
     res.redirect(`/questions/${id}`);
 }))
 
